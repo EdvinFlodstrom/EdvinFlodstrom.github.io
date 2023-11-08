c7c62e6f09d00c795eca7f3272ba9b5b4089922e
Edvin Flodström <edvin.flodstrom@gmail.com>
2023-11-08 13:04:05 +0100

    Further and more debugging

--- js.js
+++ js.js
@@ -5,8 +5,7 @@ function init() {
     const registerServiceWorker = async () => {
         if ("serviceWorker" in navigator) {
           try {
-            const registration = await navigator.serviceWorker.register("/sw.js", {
-              scope: "/",
+            const registration = await navigator.serviceWorker.register("sw.js", {
             });
             if (registration.installing) {
               console.log("Service worker installing");
