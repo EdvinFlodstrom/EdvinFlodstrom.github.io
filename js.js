1535116227a0cf6d3e9c38f091c828109a730b2b
Edvin Flodström <edvin.flodstrom@gmail.com>
2023-11-08 11:06:48 +0100

    Debugging sw.js

--- js.js
+++ js.js
@@ -2,26 +2,14 @@ window.onload = init;
 
 function init() {
 
-    const registerServiceWorker = async () => {
-        if ("serviceWorker" in navigator) {
-          try {
-            const registration = await navigator.serviceWorker.register("/sw.js", {
-              scope: "/",
-            });
-            if (registration.installing) {
-              console.log("Service worker installing");
-            } else if (registration.waiting) {
-              console.log("Service worker installed");
-            } else if (registration.active) {
-              console.log("Service worker active");
+    if('serviceWorker' in navigator) { 
+        navigator.serviceWorker.register('sw.js')
+            .then(function(registration) {
+                 console.log("Service Worker Registered");
             }
-          } catch (error) {
-            console.error(`Registration failed with ${error}`);
-          }
-        }
-      };
-            
-    registerServiceWorker();
+        ); 
+    }
+
 
     let startingCapital = 200;
     let totalChips = startingCapital;
